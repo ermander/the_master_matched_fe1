ce8c2915d7d3171205c2501f5bb94cf73f3c5fc8
Emanuele Bertuol <emanuelebertuol@gmail.com>
2021-05-10 18:51:03 +0200

    Update
    Changing the tootls-title component style

--- src/components/ToolsTitle.jsx
+++ src/components/ToolsTitle.jsx
@@ -4,7 +4,7 @@ import "../styles/_tools-title.scss"
 export default function ToolsTitle({title}) {
     return (
         <div id="tools-title">
-            <h1>{title}</h1>
+            <h2>{title}</h2>
         </div>
     )
 }

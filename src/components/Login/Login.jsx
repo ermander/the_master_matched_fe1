4587232472702f57b1be93d9f914de419950ea76
Emanuele Bertuol <emanuelebertuol@gmail.com>
2021-06-15 20:37:04 +0200

    New Login Created

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,20 +1,23 @@
 import React, { useState } from 'react';
-import { useHistory } from "react-router-dom"
-import { Container, Row, Col, Image, Form, Button, Alert } from 'react-bootstrap';
+import { useHistory, Link } from "react-router-dom"
 import { useStateValue } from "../../redux/state/stateProvider";
 import { actionTypes } from "../../redux/reducer/reducer";
 
+// BootStrap
+import { Row, Col, Button, Image, Alert } from "react-bootstrap"
+// FontAwasome
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGooglePlusG, faFacebook } from "@fortawesome/free-brands-svg-icons"
 
-import "./login.css"
+// SASS
+import "../../styles/_login.scss"
 
 function Login() {
-
     const [ email, setEmail ] = useState("")
     const [ password, setPassword ] = useState("")
     const [ show, setShow ] = useState(false)
     const history = useHistory()
     const [{}, dispatch] = useStateValue();
-
 
     const handleLogin = async () => {
         try {
@@ -44,64 +47,90 @@ function Login() {
             }else{
                 console.log(response)
                 setShow(true)
+                setTimeout(() => setShow(false), 3000)
             }     
         } catch (error) {
             console.log(error)
         }
     }
-
-    return (
+    return(
         <>
-        <Alert variant="danger" style={{textAlign: "center"}} show={show}>
-            Le tue credenziali di accesso non sono corrette! 
-           <Alert.Link href="https://the-master-matched-fe1.herokuapp.com/login"> <span style={{color: "black"}}>Clicca qui per registrarti ora!</span></Alert.Link>
-        </Alert>
-        <Container fluid className="loginContainer">
-            <Row className="no-gutters">
-                <Col xs={12} className="mt-4">
-                    <Container id="loginImageContainer">
-                        <Image id="loginLogo" src="https://res.cloudinary.com/dnadfuxk0/image/upload/v1615767803/photo_2021-03-15_01-12-53_nhmrr2.jpg" />
-                    </Container>
+            <Row className="no-gutters login-page-container">
+                <Col xs={4} className="left-side-login-container">
+                    <div className="image-container">
+                        <Image src="https://res.cloudinary.com/dnadfuxk0/image/upload/v1623768881/Il%20Diaro%20Del%20Matched%20Bettista/loghi-mb-bianco-medio_josplc.png" alt="Il Diaro Del Matched Bettista Logo" />
+                        <h5>Lorem Ipsum is simply dummy text of the <br/>printing and typesetting industry.</h5>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                            Hic perspiciatis, voluptas esse quibusdam, nihil laboriosam quasi nisi, 
+                            in magnam fugiat tempore. Reprehenderit nulla recusandae laboriosam fugiat odit
+                            sit minus nobis.
+                        </p>
+                    </div>
+                </Col>
+                <Col xs={8} className="right-side-login-container">
+                <Alert variant="danger" show={show}>
+                    Le tue credenziali di accesso non sono corrette! 
+                </Alert>
+                    <Row className="no-gutters login-form">
+                        <Col xs={3}></Col>
+                        <Col xs={6}>
+                            <Row className="no-gutters small-screen-logo">
+                                <Col xs={12}>
+                                    <Image src="https://res.cloudinary.com/dnadfuxk0/image/upload/v1623777903/Il%20Diaro%20Del%20Matched%20Bettista/logTavola_disegno_9_copia_2_e5ts6b.png" alt="Il Diario Del Matched Bettista Logo" />
+                                </Col>
+                            </Row>
+                            <Row className="first-row">
+                                <Col xs={12}>
+                                    <p>Login</p>
+                                    <Link to="/registration">Registration</Link>
+                                </Col>
+                            </Row>
+                            <Row className="inputs-row">
+                                <Col xs={12}>
+                                    <input
+                                        type="text"
+                                        placeholder="Email..."
+                                        onChange={e => setEmail(e.currentTarget.value)}
+                                    />
+                                </Col>
+                            </Row>
+                            <Row className="inputs-row">
+                                <Col xs={12}>
+                                    <input 
+                                        type="password"
+                                        placeholder="Password..."
+                                        onChange={e => setPassword(e.currentTarget.value)}
+                                        onKeyPress={handleLogin}
+                                    />
+                                </Col>
+                            </Row>
+                            <Row className="button-row">
+                                <Col xs={12}>
+                                    <Button
+                                    onClick={handleLogin}
+                                    >
+                                        Login
+                                    </Button>
+                                    <p>Password dimenticata?</p>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col xs={12} className="alternative-login">
+                                    <p>Oppure accedi con</p>
+                                    <span className="alternative-icons-container">
+                                        <FontAwesomeIcon icon={faGooglePlusG} id="google-icon"/>
+                                        <FontAwesomeIcon icon={faFacebook} id="facebook-icon"/>
+                                    </span>
+                                </Col>
+                            </Row>
+                        </Col>
+                        <Col xs={3}></Col>
+                    </Row>
                 </Col>
             </Row>
-            <Row className="mt-3 no-gutters">
-                <Col xs={12}>
-                    <Container style={{textAlign: "center"}}>
-                        <Form.Group className="loginForm">
-                            <Form.Label> </Form.Label>
-                            <Form.Control type="email" placeholder="Inserire la email" onChange={e => setEmail(e.currentTarget.value)} />
-                        </Form.Group>
-                    </Container>                        
-                </Col>
-            </Row>
-            <Row className="no-gutters">
-                <Col xs={12}>
-                    <Container style={{textAlign: "center"}}>
-                        <Form.Group className="loginForm">
-                            <Form.Label> </Form.Label>
-                            <Form.Control type="password" placeholder="Inserire la password" onChange={e => setPassword(e.currentTarget.value)}/>
-                        </Form.Group>
-                    </Container>                        
-                </Col>
-            </Row>
-            <Row className="no-gutters">
-                <Col xs={12}>
-                    <Container style={{textAlign: "center"}}>
-                        <Button 
-                            id="registrationButton" 
-                            value="login" 
-                            className="mt-3" 
-                            variant="dark"
-                            onClick={handleLogin}
-                        >
-                            Log In!
-                        </Button>
-                    </Container>
-                </Col>
-            </Row>   
-        </Container>
         </>
-    );
+    )
 }
 
-export default(Login);
+export default(Login)
